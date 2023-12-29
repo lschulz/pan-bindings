8bda3ec7f7a582f9cc88fcc2a4611abf562b9b3d
amdfxlucas <saculolissat@gmx.de>
2023-12-29 10:56:43 +0000

    use published scion-apps fork instead of local  checkout

--- build.rs
+++ build.rs
@@ -54,7 +54,6 @@ fn main() {
 
     let out_dir = env::var("OUT_DIR").unwrap();
 
-    // This is the directory where the `c` library is located.
     let libdir_path = PathBuf::from("./tmp")
         // Canonicalize the path as `rustc-link-search` requires an absolute
         // path.
@@ -62,7 +61,7 @@ fn main() {
         .expect("cannot canonicalize path");
 
     println!("libdir_path: {}", libdir_path.display());
-    // This is the path to the `c` headers file.
+
     let headers_path_str = "./include/pan/go_handle.hpp"; // headers_path.to_str().expect("Path is not a valid string");
 
     // This is the path to the intermediate object file for our library.
@@ -71,6 +70,7 @@ fn main() {
     let lib_path = libdir_path.join("libgo_handle.a");
 
     //  clang -c -o tmp/go_handle.o ../cpp/go_handle.cpp -I ../include
+    // TODO: replace this with cc
     let res = std::process::Command::new("clang++")
         .arg("-c")
         .arg("-I")
@@ -82,17 +82,12 @@ fn main() {
         .arg("./cpp/go_handle.cpp")
         .arg("-DBINDGEN")
         .output();
-    // if !res.expect("cannot execute clang").status.success()
 
     match res {
         Ok(o) => {
             print!("output: {:?}\n", o);
         }
         Err(e) => {
-            // doesnt work for some reason :(
-            // you have to enter the above command manually
-
-            //panic!("could not compile object file: {}",res.err().unwrap());
             panic!("could not compile object file: {}", e);
         }
     }
@@ -107,27 +102,16 @@ fn main() {
         panic!("could not emit library file: {}", res1.err().unwrap());
     }
 
-    /* match fs::copy("../build/go/libpand.a","tmp/libpand.a")
-    {
-        Err( err ) => { panic!("could not copy file"); }
-        Ok( o) =>{}
-    }
-    */
-
-    // println!("cargo:rustc-link-search=all={},../build/go", libdir_path.to_str().unwrap());
-
     let mut pan_path = find_file("libpan.a").unwrap();
     pan_path.pop();
-    println!("PAN_PATH: {}",pan_path.to_str().unwrap() );
+    println!("PAN_PATH: {}", pan_path.to_str().unwrap());
 
     println!("cargo:rustc-link-search=all=tmp");
     println!("cargo:rustc-link-search=all={}", &out_dir);
-    println!("cargo:rustc-link-search=all={}", pan_path.to_str().unwrap() );
+    println!("cargo:rustc-link-search=all={}", pan_path.to_str().unwrap());
     println!("cargo:rustc-link-search=all={}/lib", &out_dir);
     println!("cargo:rustc-link-search=all={}/build/go", &out_dir);
     println!("cargo:rustc-link-search=all=/lib/x86_64-linux-gnu");
-
-    //  println!("cargo:rustc-link-search=../build/go");
 
     println!("cargo:rustc-link-lib=static:+bundle=go_handle");
     println!("cargo:rustc-link-lib=static:+bundle=pan");
@@ -145,8 +129,6 @@ fn main() {
     // to bindgen, and lets you build up options for
     // the resulting bindings.
     let bindings = bindgen::Builder::default()
-        // The input header we would like to generate
-        // bindings for.
         .clang_arg("-DBINDGEN")
         .clang_arg("-xc++")
         .clang_arg("-std=c++11")
@@ -155,15 +137,10 @@ fn main() {
         .header("./include/pan/pan_cdefs.h")
         .header("./include/pan/pan.h")
         .header(headers_path_str)
-        // Tell cargo to invalidate the built crate whenever any of the
-        // included header files changed.
         .parse_callbacks(Box::new(CargoCallbacks))
-        // Finish the builder and generate the bindings.
         .generate()
-        // Unwrap the Result and panic on failure.
         .expect("Unable to generate bindings");
 
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
     let out_path = PathBuf::from(out_dir).join("bindings.rs");
     bindings
         .write_to_file(out_path.clone())
